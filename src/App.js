fb43090ca1c5f0e90802bfcdb8f5b452c86da8e9
Johan Günther Krogevoll <johankrogevoll@gmail.com>
2020-02-03 09:23:37 +0100

    removed the need for placeholder data in initial state.

--- src/App.js
+++ src/App.js
@@ -5,30 +5,34 @@ import HistoryList from "./HistoryList.js";
 
 class App extends React.Component {
   state = {
-    history: [{ lat: 0, lng: 0, velocity: 0, altitude: 0, timestamp: 0, visibility: "?" }]
+    history: []
   };
 
   constructor(props) {
     super(props);
     setInterval(async () => {
-      fetch("https://api.wheretheiss.at/v1/satellites/25544")
-        .then(res => res.json())
-        .then(data => {
-          this.setState(prevState => ({
-            history: [
-              ...prevState.history,
-              {
-                lat: data.latitude,
-                lng: data.longitude,
-                velocity: data.velocity,
-                altitude: data.altitude,
-                timestamp: data.timestamp,
-                visibility: data.visibility
-              }
-            ]
-          }));
-        });
+      this.fetchSatelliteData();
     }, 3000);
+  }
+
+  fetchSatelliteData() {
+    fetch("https://api.wheretheiss.at/v1/satellites/25544")
+      .then(res => res.json())
+      .then(data => {
+        this.setState(prevState => ({
+          history: [
+            ...prevState.history,
+            {
+              lat: data.latitude,
+              lng: data.longitude,
+              velocity: data.velocity,
+              altitude: data.altitude,
+              timestamp: data.timestamp,
+              visibility: data.visibility
+            }
+          ]
+        }));
+      });
   }
 
   render() {
--- src/MapComponent.js
+++ src/MapComponent.js
@@ -6,9 +6,12 @@ import HistoryMarker from "./Markers/HistoryMarker";
 export default class MapComponent extends React.Component {
   render() {
     const currentData = this.props.history[this.props.history.length - 1];
-    const historyMarkers = this.props.history.map((currentData, i) => (
+    let markers = this.props.history.map((currentData, i) => (
       <HistoryMarker data={currentData} key={i} />
     ));
+    if (this.props.history.length > 0) {
+      markers.push( <SatelliteMarker data={currentData} />);
+    }
 
     return (
       <Map center={[0, 0]} zoom="4">
@@ -16,9 +19,7 @@ export default class MapComponent extends React.Component {
           attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9oYW5rcm8iLCJhIjoiY2s2MjFjb3c0MDV1eTNlbzQzdWltMGJoMiJ9.rfynyBy_0cmX7D2ONmK_Tw"
         />
-        {historyMarkers}
-        <SatelliteMarker data={currentData}
-        />
+        {markers}
       </Map>
     );
   }
